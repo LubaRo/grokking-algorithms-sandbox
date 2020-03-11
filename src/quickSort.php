f0d943d64e610f6dc174261b022ad4dc9c74b888
LubaRo <cnlmode@gmail.com>
2020-03-11 21:28:59 +0400

    add function using quick sort algorithm

--- src/quickSort.php
+++ src/quickSort.php
@@ -2,5 +2,25 @@
 
 function quickSort($arr)
 {
+    if (sizeof($arr) <= 1) {
+        return $arr;
+    }
 
+    $values = array_values($arr);
+    $pivot_key = ceil(sizeof($values) / 2);
+    $pivot = $values[$pivot_key];
+
+    $less = array_filter($values, function($num) use ($pivot) {
+        return $num < $pivot;
+    });
+
+    $greater = array_filter($values, function($num) use ($pivot) {
+        return $num > $pivot;
+    });
+
+    $equal = array_filter($values, function($num) use ($pivot) {
+        return $num == $pivot;
+    });
+
+    return array_merge(quickSort($less), $equal, quickSort($greater));
 }
--- tests/quickSortTest.php
+++ tests/quickSortTest.php
@@ -11,7 +11,17 @@ class quickSortTest extends TestCase
         $unsorted_array = [2, 8, 10, 3, 1, 84, 32];
         $sorted_array = [1, 2, 3, 8, 10, 32, 84];
 
-        //$this->assertEquals($sorted_array, quickSort($unsorted_array));
-        $this->assertEquals(1, 1);
+        $this->assertEquals($sorted_array, quickSort($unsorted_array));
+
+        $unsorted_array = [15, 9, 46, 9, 30, 18, 35, 35, 0, -23, 5];
+        $sorted_array   = [-23, 0, 5, 9, 9, 15, 18, 30, 35, 35, 46];
+
+        $this->assertEquals($sorted_array, quickSort($unsorted_array));
+
+        $this->assertEquals([], quickSort([]));
+        $this->assertEquals([5], quickSort([5]));
+        $this->assertEquals([26, 35], quickSort([35, 26]));
+        $this->assertEquals([15, 26, 35], quickSort([35, 26, 15]));
+        $this->assertEquals([15, 26, 35, 115, 125], quickSort([15, 26, 35, 115, 125]));
     }
 }
