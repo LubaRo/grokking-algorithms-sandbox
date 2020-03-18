11119276b127f2ad9991d785487c3c42210cf845
LubaRo <cnlmode@gmail.com>
2020-03-18 14:08:07 +0400

    divide selection sort into two functions

--- tests/selectionSortTest.php
+++ tests/selectionSortTest.php
@@ -6,11 +6,44 @@ use PHPUnit\Framework\TestCase;
 
 class selectionSortTest extends TestCase
 {
-    public function testSelectionSort()
+    public function testSelectionSort1()
     {
-        $unsorted_array = [2, 8, 10, 3, 1, 84, 32];
-        $sorted_array = [1, 2, 3, 8, 10, 32, 84];
+        $this->assertEquals([], selectionSort([]));
+    }
+
+    public function testSelectionSort2()
+    {
+        $this->assertEquals([2], selectionSort([2]));
+    }
+
+    public function testSelectionSort3()
+    {
+        $unsorted_array = [3, 1];
+        $sorted_array = [1, 3];
 
         $this->assertEquals($sorted_array, selectionSort($unsorted_array));
+    }
+
+    public function testSelectionSort4()
+    {
+        $unsorted_array = [2, 8, -10, 3, 1, 84, 32];
+        $sorted_array = [-10, 1, 2, 3, 8, 32, 84];
+
+        $this->assertEquals($sorted_array, selectionSort($unsorted_array));
+    }
+
+    public function testFindSmallest1()
+    {
+        $this->assertEquals([null, null], findSmallest([]));
+    }
+
+    public function testFindSmallest2()
+    {
+        $this->assertEquals([15, 0], findSmallest([15]));
+    }
+
+    public function testFindSmallest3()
+    {
+        $this->assertEquals([-457, 'z'], findSmallest(['a' => 15, 4 => 18, 'z' => -457, 'key' => 90]));
     }
 }
