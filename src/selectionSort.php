504a97885bd6a594ee7666833696305b8b087ca7
LubaRo <cnlmode@gmail.com>
2020-03-18 19:24:33 +0400

    add optimal realisation of selection sort + add comments

--- src/selectionSort.php
+++ src/selectionSort.php
@@ -1,5 +1,7 @@
 <?php
-
+/*
+    The simplest and most intuitive iterative implementation
+*/
 function selectionSort($unsortedArr)
 {
     if (count($unsortedArr) <= 1) {
@@ -18,6 +20,36 @@ function selectionSort($unsortedArr)
     return $sortedArr;
 }
 
+/*
+    The best of the given implementations
+    It doesn't require additional memory,
+    only for storing the index of current minimal value
+*/
+function selectionSortOptimal($arr)
+{
+    if (count($arr) <= 1) {
+        return $arr;
+    }
+
+    for ($i = 0; $i < count($arr) - 1; $i++) {
+        $minIndex = $i;
+        for ($j = $i + 1; $j < count($arr); $j++) {
+            if ($arr[$j] < $arr[$minIndex]) {
+                $minIndex = $j;
+            }
+        }
+
+        if ($minIndex != $i) {
+            [$arr[$minIndex], $arr[$i]] = [$arr[$i], $arr[$minIndex]];
+        }
+    }
+
+    return $arr;
+}
+
+/*
+    Recursion Implementation
+*/
 function selectionSortRecursive($unsortedArr)
 {
     if (count($unsortedArr) <= 1) {
@@ -31,6 +63,10 @@ function selectionSortRecursive($unsortedArr)
     return array_merge([$minVal], selectionSortRecursive($unsortedArr));
 }
 
+/*
+    Helper function that returns
+    the smallest array value and its index
+*/
 function findSmallest($list)
 {
     if (empty($list)) {
