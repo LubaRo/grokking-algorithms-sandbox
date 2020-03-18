e6eb52fd95d43327fa968bed0d4e4fea635e45b7
LubaRo <cnlmode@gmail.com>
2020-03-18 14:14:49 +0400

    add recursive selection sort function

--- tests/selectionSortTest.php
+++ tests/selectionSortTest.php
@@ -32,6 +32,32 @@ class selectionSortTest extends TestCase
         $this->assertEquals($sorted_array, selectionSort($unsorted_array));
     }
 
+    public function testSelectionSortRecursive1()
+    {
+        $this->assertEquals([], selectionSortRecursive([]));
+    }
+
+    public function testSelectionSortRecursive2()
+    {
+        $this->assertEquals([2], selectionSortRecursive([2]));
+    }
+
+    public function testSelectionSortRecursive3()
+    {
+        $unsorted_array = [3, 1];
+        $sorted_array = [1, 3];
+
+        $this->assertEquals($sorted_array, selectionSortRecursive($unsorted_array));
+    }
+
+    public function testSelectionSortRecursive4()
+    {
+        $unsorted_array = [2, 8, -10, 3, 1, 84, 32];
+        $sorted_array = [-10, 1, 2, 3, 8, 32, 84];
+
+        $this->assertEquals($sorted_array, selectionSortRecursive($unsorted_array));
+    }
+
     public function testFindSmallest1()
     {
         $this->assertEquals([null, null], findSmallest([]));
